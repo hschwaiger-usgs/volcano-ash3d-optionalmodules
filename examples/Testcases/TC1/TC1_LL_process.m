72b67d1439b69f423bf6bb06d21f6affdd2b4a39
Hans F. Schwaiger <hschwaiger@ramp.wr.usgs.gov>
2017-12-22 16:56:20 -0900

    Updated some of the testcase 1-3 scripts.

--- examples/Testcases/TC1/TC1_LL_process.m
+++ examples/Testcases/TC1/TC1_LL_process.m
@@ -1,10 +1,9 @@
 clear all;
 
-nresmax  = 3;
+nresmax  = 5;
 nres  = char('2000','1000','0500','0250','0125');
-
 resdx = [2.0 1.0 0.5 0.25 0.125];
-subcase = 8;
+subcase = 1;
 % Horizontal advection:
 %  subcase 1 : x+y0
 %  subcase 2 : x-y0
@@ -15,39 +14,78 @@ subcase = 8;
 %  subcase 7 : x-y+
 %  subcase 8 : x+y-
 
-for inres = 1:nresmax
+limmin=1;
+limmax=2;
+nlim = limmax-limmin+1;
+nlim_label = char('LIM_NO','LIM_LW','LIM_BW','LIM_FM','LIM_MM','LIM_SB','LIM_MC');
+
+GlobL1Errors1=zeros(nresmax,nlim);
+GlobL2Errors1=zeros(nresmax,nlim);
+GlobMCErrors1=zeros(nresmax,nlim);
+GlobL1Errors2=zeros(nresmax,nlim);
+GlobL2Errors2=zeros(nresmax,nlim);
+GlobMCErrors2=zeros(nresmax,nlim);
+
+
+for ilim = limmin:limmax
+ for inres = 1:nresmax
   dx = resdx(inres);
 
   step = 1;
-  ierfile1 = sprintf('DATA/TC1_LL_Sub%i_%s_St%i_err.dat',  ...
-                subcase,strtrim(nres(inres,:)),step);
+  ierfile1 = sprintf('DATA/TC1_LL_Sub%i_%s_%s_St%i_err.dat',  ...
+                subcase,strtrim(nlim_label(ilim,:)),strtrim(nres(inres,:)),step);
 
   L1L2errors = load(ierfile1);
-  GlobL1Errors1(inres) = L1L2errors(1);
-  GlobL2Errors1(inres) = L1L2errors(2);
-  GlobMCErrors1(inres) = L1L2errors(3);
+  GlobL1Errors1(inres,ilim) = L1L2errors(1);
+  GlobL2Errors1(inres,ilim) = L1L2errors(2);
+  GlobMCErrors1(inres,ilim) = L1L2errors(3);
 
   step = 2;
-  ierfile2 = sprintf('DATA/TC1_LL_Sub%i_%s_St%i_err.dat',  ...
-                subcase,strtrim(nres(inres,:)),step);
+  ierfile2 = sprintf('DATA/TC1_LL_Sub%i_%s_%s_St%i_err.dat',  ...
+                subcase,strtrim(nlim_label(ilim,:)),strtrim(nres(inres,:)),step);
 
   L1L2errors = load(ierfile2);
-  GlobL1Errors2(inres) = L1L2errors(1);
-  GlobL2Errors2(inres) = L1L2errors(2);
-  GlobMCErrors2(inres) = L1L2errors(3);
-
+  GlobL1Errors2(inres,ilim) = L1L2errors(1);
+  GlobL2Errors2(inres,ilim) = L1L2errors(2);
+  GlobMCErrors2(inres,ilim) = L1L2errors(3);
+ end
 end
 
+%Specify point of O(1) and O(2) intersection
+Ox=1.0e1;
+Oy=1.0e-4;
+dxmin=1.0e-1;
+dxmax=1.0e1;
+L1min=1.0e-8;
+L1max=1.0e-4;
+L2min=1.0e-11;
+L2max=1.0e-7;
+Ermin=1.0e-16;
+Ermax=1.0e-13;
 figure;
-subplot(1,3,1),loglog(resdx(1:nresmax),GlobL1Errors1(1:nresmax),'b-',resdx(1:nresmax),GlobL1Errors2(1:nresmax),'r-')
-ylabel('L1 Error')
+% Interior
+subplot(2,3,1),loglog([Ox,Ox*1.0e-2],[Oy,Oy*1.0e-2],'k--');hold on;
+subplot(2,3,1),loglog([Ox,Ox*1.0e-2],[Oy,Oy*1.0e-4],'k:')
+subplot(2,3,1),loglog(resdx(1:nresmax),GlobL1Errors1(1:nresmax,:));axis([dxmin dxmax L1min L1max]);hold off;
+ylabel('L_1 Error')
 xlabel('dx')
-legend('Stp1-Inter','Stp2-Bound')
-subplot(1,3,2),loglog(resdx(1:nresmax),GlobL2Errors1(1:nresmax),'b-',resdx(1:nresmax),GlobL2Errors2(1:nresmax),'r-')
-ylabel('L2 Error')
+%legend('O(1)','O(2)','None','Lax-Wen','BeamWarm','Fromm','MinMod','Superbee','MC','Location','northwest');
+subplot(2,3,2),loglog(resdx(1:nresmax),GlobL2Errors1(1:nresmax,:));axis([dxmin dxmax L2min L2max]);
+ylabel('L_2 Error')
 xlabel('dx')
-subplot(1,3,3),loglog(resdx(1:nresmax),GlobMCErrors1(1:nresmax),'b-',resdx(1:nresmax),GlobMCErrors1(1:nresmax),'bo');hold on;
-subplot(1,3,3),loglog(resdx(1:nresmax),GlobMCErrors2(1:nresmax),'r-',resdx(1:nresmax),GlobMCErrors2(1:nresmax),'ro');hold off;
+subplot(2,3,3),loglog(resdx(1:nresmax),GlobMCErrors1(1:nresmax,:));axis([dxmin dxmax Ermin Ermax]);
 ylabel('MC Error')
 xlabel('dx')
 
+% Boundary
+subplot(2,3,4),loglog([Ox,Ox*1.0e-2],[Oy,Oy*1.0e-2],'k--');hold on;
+subplot(2,3,4),loglog([Ox,Ox*1.0e-2],[Oy,Oy*1.0e-4],'k:')
+subplot(2,3,4),loglog(resdx(1:nresmax),GlobL1Errors2(1:nresmax,:));axis([dxmin dxmax L1min L1max]);hold off;
+ylabel('L_1 Error')
+xlabel('dx')
+subplot(2,3,5),loglog(resdx(1:nresmax),GlobL2Errors2(1:nresmax,:));axis([dxmin dxmax L2min L2max]);
+ylabel('L_2 Error')
+xlabel('dx')
+subplot(2,3,6),loglog(resdx(1:nresmax),GlobMCErrors2(1:nresmax,:));axis([dxmin dxmax Ermin Ermax]);
+ylabel('MC Error')
+xlabel('dx')
--- examples/Testcases/TC1/TC1_xy_process.m
+++ examples/Testcases/TC1/TC1_xy_process.m
@@ -1,14 +1,9 @@
 clear all;
 
-nresmax  = 3;
+nresmax  = 4;
 nres  = char('50000','25000','12500','06250','03125');
-
 resdx = [0.05000 0.025000 0.012500 0.006250 0.003125];
-subcase = 8;
-
-nlim = 7;
-nlim_label = char('LIM_NO','LIM_LW','LIM_BW','LIM_FM','LIM_MM','LIM_SB','LIM_MC');
-
+subcase = 1;
 % Horizontal advection:
 %  subcase 1 : x+y0
 %  subcase 2 : x-y0
@@ -19,6 +14,11 @@ nlim_label = char('LIM_NO','LIM_LW','LIM_BW','LIM_FM','LIM_MM','LIM_SB','LIM_MC'
 %  subcase 7 : x-y+
 %  subcase 8 : x+y-
 
+limmin=1;
+limmax=2;
+nlim = limmax-limmin+1;
+nlim_label = char('LIM_NO','LIM_LW','LIM_BW','LIM_FM','LIM_MM','LIM_SB','LIM_MC');
+
 GlobL1Errors1=zeros(nresmax,nlim);
 GlobL2Errors1=zeros(nresmax,nlim);
 GlobMCErrors1=zeros(nresmax,nlim);
@@ -27,7 +27,7 @@ GlobL2Errors2=zeros(nresmax,nlim);
 GlobMCErrors2=zeros(nresmax,nlim);
 
 
-for ilim = 1:nlim
+for ilim = limmin:limmax
  for inres = 1:nresmax
   dx = resdx(inres);
 
@@ -51,6 +51,9 @@ for ilim = 1:nlim
  end
 end
 
+%Specify point of O(1) and O(2) intersection
+Ox=1.0e-1;
+Oy=1.0e-1;
 dxmin=1.0e-3;
 dxmax=1.0e-1;
 L1min=1.0e-3;
@@ -59,29 +62,31 @@ L2min=1.0e-2;
 L2max=1.0e0;
 Ermin=1.0e-16;
 Ermax=1.0e-13;
-figure(1);
+
+figure;
 % Interior
-subplot(1,3,1),loglog(resdx(1:nresmax),GlobL1Errors1(1:nresmax,:));axis([dxmin dxmax L1min L1max]);
-ylabel('L1 Error')
+subplot(2,3,1),loglog([Ox,Ox*1.0e-2],[Oy,Oy*1.0e-2],'k--');hold on;
+subplot(2,3,1),loglog([Ox,Ox*1.0e-2],[Oy,Oy*1.0e-4],'k:')
+subplot(2,3,1),loglog(resdx(1:nresmax),GlobL1Errors1(1:nresmax,:));axis([dxmin dxmax L1min L1max]);hold off;
+ylabel('L_1 Error')
 xlabel('dx')
-legend('None','Lax-Wen','BeamWarm','Fromm','MinMod','Superbee','MC');
-subplot(1,3,2),loglog(resdx(1:nresmax),GlobL2Errors1(1:nresmax,:));axis([dxmin dxmax L2min L2max]);
-ylabel('L2 Error')
+%legend('O(1)','O(2)','None','Lax-Wen','BeamWarm','Fromm','MinMod','Superbee','MC','Location','northwest');
+subplot(2,3,2),loglog(resdx(1:nresmax),GlobL2Errors1(1:nresmax,:));axis([dxmin dxmax L2min L2max]);
+ylabel('L_2 Error')
 xlabel('dx')
-subplot(1,3,3),loglog(resdx(1:nresmax),GlobMCErrors1(1:nresmax,:));axis([dxmin dxmax Ermin Ermax]);
+subplot(2,3,3),loglog(resdx(1:nresmax),GlobMCErrors1(1:nresmax,:));axis([dxmin dxmax Ermin Ermax]);
 ylabel('MC Error')
 xlabel('dx')
 
-figure(2)
 % Boundary
-subplot(1,3,1),loglog(resdx(1:nresmax),GlobL1Errors2(1:nresmax,:));axis([dxmin dxmax L1min L1max]);
-ylabel('L1 Error')
+subplot(2,3,4),loglog([Ox,Ox*1.0e-2],[Oy,Oy*1.0e-2],'k--');hold on;
+subplot(2,3,4),loglog([Ox,Ox*1.0e-2],[Oy,Oy*1.0e-4],'k:')
+subplot(2,3,4),loglog(resdx(1:nresmax),GlobL1Errors2(1:nresmax,:));axis([dxmin dxmax L1min L1max]);hold off;
+ylabel('L_1 Error')
 xlabel('dx')
-subplot(1,3,2),loglog(resdx(1:nresmax),GlobL2Errors2(1:nresmax,:));axis([dxmin dxmax L2min L2max]);
-ylabel('L2 Error')
+subplot(2,3,5),loglog(resdx(1:nresmax),GlobL2Errors2(1:nresmax,:));axis([dxmin dxmax L2min L2max]);
+ylabel('L_2 Error')
 xlabel('dx')
-subplot(1,3,3),loglog(resdx(1:nresmax),GlobMCErrors2(1:nresmax,:));axis([dxmin dxmax Ermin Ermax]);
+subplot(2,3,6),loglog(resdx(1:nresmax),GlobMCErrors2(1:nresmax,:));axis([dxmin dxmax Ermin Ermax]);
 ylabel('MC Error')
 xlabel('dx')
-
-
