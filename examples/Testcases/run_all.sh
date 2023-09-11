bbd1026c42a226dcfa381294185ed0134f79deb2
Hans <hschwaiger@usgs.gov>
2023-09-10 22:10:55 -0800

    Added better control of what tests are run in run_all.sh

--- examples/Testcases/run_all.sh
+++ examples/Testcases/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TestcasesDir=`pwd`
 il=1 # 0 is just no-limiter, 1 in NO and Superbee, 6 is all
-ix=2 # 5 is the max coded but that takes overnight
+ix=3 # 5 is the max coded but that takes overnight
 
 #il=6 # 0 is just no-limiter, 1 in NO and Superbee, 6 is all
 #ix=5 # 5 is the max coded but that takes overnight
@@ -18,7 +18,7 @@ fi
     #  1 2 3 4 5
 cases=(1 0 0 0 0)
 
-if [ $[cases[0]] -eq 1 ]; then
+if [ "$[cases[0]]" -eq 1 ]; then
   cd ${TestcasesDir}/TC1
   limmax=$il idx=$ix time ./TC1_XY_run
   ln -s TC1_XY_process.m TC1_XY_process
@@ -29,7 +29,7 @@ if [ $[cases[0]] -eq 1 ]; then
   cd ${TestcasesDir}
 fi
 
-if [ $[cases[1]] -eq 1 ]; then
+if [ "$[cases[1]]" -eq 1 ]; then
   cd ${TestcasesDir}/TC2
   limmax=$il idx=$ix time ./TC2_XY_run
   ln -s TC2_XY_process.m TC2_XY_process
@@ -37,7 +37,7 @@ if [ $[cases[1]] -eq 1 ]; then
   cd ${TestcasesDir}
 fi
 
-if [ $[cases[2]] -eq 1 ]; then
+if [ "$[cases[2]]" -eq 1 ]; then
   cd ${TestcasesDir}/TC3
   limmax=$il idx=$ix time ./TC3_XY_run
   ln -s TC3_XY_process.m TC3_XY_process
@@ -48,7 +48,7 @@ if [ $[cases[2]] -eq 1 ]; then
   cd ${TestcasesDir}
 fi
 
-if [ $[cases[3]] -eq 1 ]; then 
+if [ "$[cases[3]]" -eq 1 ]; then 
   cd ${TestcasesDir}/TC4
   limmax=$il idx=$ix time ./TC4_XY_run
   ln -s TC4_XY_process.m TC4_XY_process
@@ -56,7 +56,7 @@ if [ $[cases[3]] -eq 1 ]; then
   cd ${TestcasesDir}
 fi
 
-if [ $[cases[4]] -eq 1 ]; then
+if [ "$[cases[4]]" -eq 1 ]; then
   cd ${TestcasesDir}/TC5
   limmax=$il idx=$ix time ./TC5_XY_run
   ln -s TC5_XY_process.m TC5_XY_process
@@ -68,7 +68,7 @@ if [ $[cases[4]] -eq 1 ]; then
 fi
 
 # Reporting on how we did
-if [ $[cases[0]] -eq 1 ]; then
+if [ "$[cases[0]]" -eq 1 ]; then
   echo "------------------------------------------------------------------------------"
   echo "     Test Case 1: Horizontal advection"
   echo "------------------------------------------------------------------------------"
@@ -78,7 +78,7 @@ if [ $[cases[0]] -eq 1 ]; then
   cat TC1/DATA/TC1_ConvRate_LL.dat
 fi
 
-if [ $[cases[1]] -eq 1 ]; then
+if [ "$[cases[1]]" -eq 1 ]; then
   echo "------------------------------------------------------------------------------"
   echo "     Test Case 2: Vertical advection"
   echo "------------------------------------------------------------------------------"
@@ -86,7 +86,7 @@ if [ $[cases[1]] -eq 1 ]; then
   cat TC2/DATA/TC2_ConvRate_XY.dat
 fi
 
-if [ $[cases[2]] -eq 1 ]; then
+if [ "$[cases[2]]" -eq 1 ]; then
   echo "------------------------------------------------------------------------------"
   echo "     Test Case 3: Horizontal box/cone rigid rotation"
   echo "------------------------------------------------------------------------------"
@@ -96,7 +96,7 @@ if [ $[cases[2]] -eq 1 ]; then
   cat TC3/DATA/TC3_ConvRate_LL.dat
 fi
 
-if [ $[cases[3]] -eq 1 ]; then
+if [ "$[cases[3]]" -eq 1 ]; then
   echo "------------------------------------------------------------------------------"
   echo "     Test Case 4: Diffusion (Expl. and Crank-Nic.)"
   echo "------------------------------------------------------------------------------"
@@ -104,7 +104,7 @@ if [ $[cases[3]] -eq 1 ]; then
   cat TC4/DATA/TC4_ConvRate_XY.dat
 fi
 
-if [ $[cases[4]] -eq 1 ]; then
+if [ "$[cases[4]]" -eq 1 ]; then
   echo "------------------------------------------------------------------------------"
   echo "     Test Case 5: Horizontal shear rotation"
   echo "------------------------------------------------------------------------------"
