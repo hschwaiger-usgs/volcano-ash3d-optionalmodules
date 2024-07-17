7b65f8a4148efea90bf941b59e23265ff6c3ba4c
Hans <hschwaiger@usgs.gov>
2024-07-16 22:07:32 -0800

    Minor synchronization of top file.

--- src/Ash3d_TC.F90
+++ src/Ash3d_TC.F90
@@ -44,7 +44,7 @@
          ivent,jvent,nxmax,nymax,nzmax,nsmax,ts0,ts1,ZPADDING,dz_vec_pd,z_cc_pd
 
       use solution,      only : &
-         concen_pd,DepositGranularity,StopValue,aloft_percent_remaining, &
+         concen_pd,DepositGranularity,StopValue_FracAshDep,aloft_percent_remaining, &
          SourceCumulativeVol,dep_vol,aloft_vol,outflow_vol,tot_vol,vf_pd
 
       use Output_Vars,   only : &
@@ -139,8 +139,8 @@
       real(kind=dp)         :: dt_TC
 
       INTERFACE
-        subroutine input_data_ResetParams
-        end subroutine input_data_ResetParams
+!        subroutine input_data_ResetParams
+!        end subroutine input_data_ResetParams
         subroutine alloc_arrays
         end subroutine alloc_arrays
         subroutine calc_mesh_params
@@ -206,12 +206,12 @@
         do io=1,2;if(VB(io).le.verbosity_essential)then
           write(outlog(io),*)"Testing for ",OPTMOD_names(i),i
         endif;enddo
-        if(OPTMOD_names(i).eq.'RESETPARAMS')then
-          do io=1,2;if(VB(io).le.verbosity_essential)then
-            write(outlog(io),*)"  Reading input block for RESETPARAMS"
-          endif;enddo
-          call input_data_ResetParams
-        endif
+        !if(OPTMOD_names(i).eq.'RESETPARAMS')then
+        !  do io=1,2;if(VB(io).le.verbosity_essential)then
+        !    write(outlog(io),*)"  Reading input block for RESETPARAMS"
+        !  endif;enddo
+        !  call input_data_ResetParams
+        !endif
       enddo
       do io=1,2;if(VB(io).le.verbosity_info)then    
         write(outlog(io),*)"Finished reading all specialized input blocks"
@@ -638,7 +638,7 @@
         ! Check stop conditions
         !  If any of these is true, then the time loop will stop
            ! Stops if there is less than 1% of ash aloft in the domain
-        StopConditions(1) = (aloft_percent_remaining.lt.(1.0_ip-StopValue))
+        StopConditions(1) = (aloft_percent_remaining.lt.(1.0_ip-StopValue_FracAshDep))
            ! Normal stop condition if simulation exceeds alloted time
         StopConditions(2) = (time.ge.Simtime_in_hours)
            ! Normal stop condition when nothing is left to advect
@@ -675,7 +675,7 @@
           StopTimeLoop = .false.
         endif
       enddo  !loop over itime
-              !  ((dep_percent_accumulated.le.StopValue).and. &
+              !  ((dep_percent_accumulated.le.StopValue_FracAshDep).and. &
               !    (time.lt.Simtime_in_hours)        .and. &
               !    (n_gs_aloft.gt.0))
 
@@ -689,7 +689,7 @@
          (StopConditions(1).eqv..true.))then
         ! Normal stop condition set by user tracking the deposit
         do io=1,2;if(VB(io).le.verbosity_info)then
-          write(outlog(io),'(a35,f8.3)')"Percent accumulated/exited exceeds ",StopValue
+          write(outlog(io),'(a35,f8.3)')"Percent accumulated/exited exceeds ",StopValue_FracAshDep
         endif;enddo
       endif
       if((CheckConditions(2).eqv..true.).and.&
@@ -746,7 +746,7 @@
       do io=1,2;if(VB(io).le.verbosity_info)then
         write(outlog(io),5012)   ! put footnotes below output table
         write(outlog(io),'(a5,f10.3,a5,f10.3)')'time=',time,', dt=',dt
-        write(outlog(io),'(a26,f15.5)')"Mass Conservation Error = ",MassConsErr
+        write(outlog(io),'(a26,g15.5)')"Mass Conservation Error = ",MassConsErr
       endif;enddo
 
         ! Make sure we have the latest output variables and go to write routines
